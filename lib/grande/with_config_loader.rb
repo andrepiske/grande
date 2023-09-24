a8c0bf5e5b6338c2a00a9fa749cdbca736cbaa60
André Diego Piske <andrepiske@gmail.com>
2023-09-24 12:01:05 -0300

    Return config

--- lib/grande/with_config_loader.rb
+++ lib/grande/with_config_loader.rb
@@ -3,8 +3,10 @@ module Grande::WithConfigLoader
   def with_config_loader
     @config_loader = Grande::ConfigLoader.new
 
-    yield
+    result = yield
 
     @config_loader = nil # Config doesn't need to be kept in memory after it's loaded. Free up some memory here
+
+    result
   end
 end
